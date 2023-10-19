2c63913c7c2ff6f2162e99c6c3f0147046ddbc3d
JEHFxD <148361405+JEHFxD@users.noreply.github.com>
2023-10-19 08:47:51 -0500

    Subir archivos

---
+++ Javascript/Intro/Variables/Ejercicios/Ejercicio3.js
@@ -0,0 +1,9 @@
+let numero = 50
+console.log(numero)
+console.log(numero)
+console.log(numero)
+console.log(numero)
+document.getElementById(numero).innerHTML = numero
+document.getElementById("502").innerHTML = numero
+document.getElementById("503").innerHTML = numero
+document.getElementById("504").innerHTML = numero
---
+++ Javascript/Intro/Variables/Ejercicios/Ejercicio4.js
@@ -0,0 +1,5 @@
+let numero= prompt ("inserte el valor de la gravedad")
+if(confirm("El valor de la gravedad")){
+    const gravedad = numero
+    alert(gravedad)
+}
