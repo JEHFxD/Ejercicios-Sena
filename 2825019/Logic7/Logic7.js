a782f129fef6110bc064395e8ea2eb3d6db4ed5e
JEHFxD <148361405+JEHFxD@users.noreply.github.com>
2023-11-01 10:46:04 -0600

    Add files via upload

---
+++ 2825019/Logic7/Logic7.js
@@ -0,0 +1,7 @@
+let number1 = parseInt(prompt("Ingrese numero"))
+if (!isNaN(number1)) {
+let number2 = parseInt(prompt("Ingrese numero"))
+if (!isNaN(number2)){
+let number3 = parseInt(prompt("Ingrese numero"))    
+}else console.log("El valor agregado es incorrecto")
+}else console.log("El valor agregado es incorrecto")
