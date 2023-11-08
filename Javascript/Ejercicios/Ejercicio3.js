b58747c4de1c3b057b30bc7bf4cc16ad5dd4bdd8
JEHFxD <148361405+JEHFxD@users.noreply.github.com>
2023-11-08 10:19:00 -0500

    Delete Javascript directory

--- Javascript/Ejercicios/Ejercicio3.js
+++
@@ -1,9 +0,0 @@
-let numero = 50
-console.log(numero)
-console.log(numero)
-console.log(numero)
-console.log(numero)
-document.getElementById(numero).innerHTML = numero
-document.getElementById("502").innerHTML = numero
-document.getElementById("503").innerHTML = numero
-document.getElementById("504").innerHTML = numero
--- Javascript/Intro/Variables/Ejercicios/Ejercicio2.js
+++
@@ -1,7 +0,0 @@
-const pi=3.14
-const URL="http://www.google.com"
-console.log(pi)
-console.log(URL)
-document.getElementById("pi").innerHTML=pi
-document.getElementById("URL").innerHTML=URL
-
