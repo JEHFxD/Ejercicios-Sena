646f066ff33d128690d26001448878fa739b56bd
JEHFxD <148361405+JEHFxD@users.noreply.github.com>
2023-11-08 10:21:14 -0500

    Delete condicionales directory

--- condicionales/Ejercicio1/Ejercicio1.js
+++
@@ -1,8 +0,0 @@
-let angulo1 = parseFloat(prompt("Ingrese el angulo"))
-let angulo2 = parseFloat(prompt("Ingrese el angulo"))
-let angulo3 = parseFloat(prompt("Ingrese el angulo"))
-if (angulo1+angulo2+angulo3==100){
-    console.log("Los angulos si correspondena a los de un triangulo")
-}else{
-    console.log("Los angulos no corresponden a los de un triangulo")
-}
--- condicionales/Ejercicio2/Ejercicio2.js
+++
@@ -1,6 +0,0 @@
-let comida = prompt("Ingrese comida")
-if ((comida == "lenteja") || (comida == "arroz")){
-    console.log("No paga IVA")
-}else if ((comida == "crema") || (comida == "vino")){
-    console.log("Paga IVA")
-}
