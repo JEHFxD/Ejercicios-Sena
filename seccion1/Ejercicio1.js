a3eef6ae9aae4a6aecd97868566b085a5247deb2
JEHFxD <148361405+JEHFxD@users.noreply.github.com>
2023-11-08 10:21:32 -0500

    Delete seccion1 directory

--- seccion1/Ejercicio1.js
+++
@@ -1,4 +0,0 @@
-let edad = parseInt(prompt("Ingrese su edad"))
-let años= parseInt(prompt("Años"))
-let edad2 = edad + años
-console.log("la edad del usuario sera de:" , edad2);
--- seccion1/Ejercicio2.js
+++
@@ -1,7 +0,0 @@
-let numero1 = parseFloat(prompt("Ingrese un numero"));
-let numero2 = parseFloat(prompt("Ingrese un numero"))
-console.log("la suma es de:", numero1+numero2)
-console.log("la resta es de:",numero1-numero2)
-console.log("el producto es:",numero1*numero2)
-console.log("el cociente es:", numero1/numero2)
-console.log("el modulo es de:",numero1%numero2)
